41dc66ddedb1bf6ee9522eaff977d91b91326416
raizacoelho <rcrosa@inf.ufpel.edu.br>
2023-07-12 23:08:48 -0300

    lista 1

---
+++ Programacao_Descomplicada/Lista1/L1E46.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    
+    
+    return 0;
+}
+
+Fac¸a um programa que leia um numero inteiro positivo de tr ´ es d ˆ ´ıgitos (de 100 a 999).
+Gere outro numero formado pelos d ´ ´ıgitos invertidos do numero lido. 
